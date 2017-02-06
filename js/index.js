3124e37a6df18db667a352bff570c6a5058844c9
Mykhailo <plyuta.mykhailo@gmail.com>
2017-02-06 13:58:15 +0000

    alpha

--- js/index.js
+++ js/index.js
@@ -4,6 +4,59 @@ $(document).ready(function(){
      
       slidesToShow: 3,
       autoplay: true,
-      autoplaySpeed: 3000
+      autoplaySpeed: 3000,
+      
+      responsive: [
+    {
+      breakpoint: 1290,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 860,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+   ]
+      
   });
+  
+  $('.slider-partners').slick({
+    infinite: true,
+     
+      slidesToShow: 8,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+    {
+      breakpoint: 1290,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 890,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+   ]
+      
+  });
+  
+   
+  
 });
